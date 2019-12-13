ed8dfa74739cd7f8c5b2f9182a8d49e9176f3501
Wang <WangKe@morton709.org>
2019-12-13 09:35:50 -0600

    safsafasfadfa

--- cereal.java
+++ cereal.java
@@ -1,33 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-public class cereal{
+public class cereal extends cerealInfo{
   public static void main(String[] args){
-
-  }
-  public class cerealInfo{
-    private double rating;
-    private int shelf;
-    private String cerealName;
-    String csvFile = "C:/Users/wangke/Desktop/Examples/lab2data-kevroch/Cereal.csv";
-
-    int column = 0;
-    try{
-    Scanner cerealScan = new Scanner(new FileReader(csvFile));
-
-
-    public void columns(){
-        while(cerealScan.hasNext()){
-          column++;
-
-
-        }
-      }
-      catch(Exception e){
-        System.out.println(e);
-      }
-    return column;
-    }
+    System.out.println(cerealInfo.columns());
   }
 }
 
