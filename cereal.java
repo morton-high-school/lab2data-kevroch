1022edb494a1fb7e038075afc2a1f781c1a7fa18
Wang <WangKe@morton709.org>
2019-12-18 09:19:22 -0600

    sdfs

--- cereal.java
+++ cereal.java
@@ -1,29 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-public class cereal extends cerealInfo{
+public class cereal{
   public static void main(String[] args){
-    System.out.println(cerealInfo.columns());
+    cerealInfo t = new cerealInfo();
+    System.out.println(t.Column());
   }
 }
-
-
-
-/*public class cerealInfo{
-  private double rating;
-  private int shelf;
-  private String cerealName;
-  String csvFile = "C:/Users/wangke/Desktop/Examples/lab2data-kevroch/Cereal.csv";
-
-  int column = 0;
-  Scanner cerealScan = new Scanner(new filereader(csvFile));
-
-  public int columns(int column){
-    while(cerealScan.hasNext()){
-      column++;
-    }
-  }
-
-
-
-}*/
--- cerealInfo.java
+++ cerealInfo.java
@@ -1,26 +1,44 @@
 import java.util.*;
 import java.io.*;
+import java.io.FileReader.*;
 
-public abstract class cerealInfo{
+public class cerealInfo{
   private double rating;
   private int shelf;
   private String cerealName;
-  public static String csvFile = "C:/Users/wangke/Desktop/Examples/lab2data-kevroch/Cereal.csv";
+  String csvFile = "C:/Users/wangke/Desktop/Examples/lab2data-kevroch/Cereal.csv";
 
-  public static int column = 0;
+  private int column = 0;
 
+  //Scanner cerealScan = new Scanner(new FileReader(csvFile));
 
-
-    public static int columns(){
-      try{
-        Scanner cerealScan = new Scanner(new FileReader(csvFile));
-      while(cerealScan.hasNext()){
+  public int Column(){
+    try{
+      Scanner cerealScan = new Scanner(new FileReader(csvFile));
+      while(cerealScan.hasNextLine()){
+        cerealScan.next();
         column++;
-      }
-
-  }catch(Exception e){
+        }
+    }catch(Exception e){
       System.out.println(e);
     }
-    return column;
+      return column;
+    }
+    public String shelfName(){
+      String temp = " ";
+      try{
+        Scanner nameScan = new Scanner(new FileReader(csvFile));
+        while(nameScan.hasNextLine()){
+          String temp[] = nameScan.nextLine().split(",");
+
+        }catch(Exception e){
+          System.out.println(e);
+        }
+
+      }
+
+      }
+    }
+
+
   }
-}
